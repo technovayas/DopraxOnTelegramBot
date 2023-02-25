2742c37ea5e8b9233e360b666b22d3bf465164fb
technovayas <126324499+technovayas@users.noreply.github.com>
2023-02-25 16:01:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone https://github.com/lazygramyt/File-Sharing-Bot ok && cd ok && pip3 install -U -r requirements.txt
+git clone https://github.com/technovayas/File-Sharing-Bot ok && cd ok && pip3 install -U -r requirements.txt
 python3 main.py
